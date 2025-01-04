d8337bb57cb9cdad7fe5c6c0e8d253eceb86605b
ValliReddy <157823804+ValliReddy@users.noreply.github.com>
2025-01-04 06:44:30 -0800

    Env

--- chatapp/src/components/firebaseConfig.js
+++ chatapp/src/components/firebaseConfig.js
@@ -1,25 +1,24 @@
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
-import { getAuth, setPersistence, browserLocalPersistence, browserSessionPersistence } from "firebase/auth";
+import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7zpIK13BbmCjN8TfwwF4k90qPJoRyvJs",
-    authDomain: "chatterbox-cc56d.firebaseapp.com",
-    projectId: "chatterbox-cc56d",
-    storageBucket: "chatterbox-cc56d.firebasestorage.app",
-    messagingSenderId: "91887160628",
-    appId: "1:91887160628:web:c407d71911a63fef07e961",
-    measurementId: "G-QWKDQ1RMF5"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// export { auth, db };
+// Set persistence to session
 setPersistence(auth, browserSessionPersistence)
   .then(() => {
     console.log("Firebase Authentication session persistence set to session");
